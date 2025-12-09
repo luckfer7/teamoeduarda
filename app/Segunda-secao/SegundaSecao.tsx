cc959fbb5e28a1199c5191bfff54476c97263800
lucas camilo vonlabs <lucas.camilo@vonbraunlabs.com.br>
2025-12-09 16:56:28 -0300

    adição de imagens

--- app/Segunda-secao/SegundaSecao.tsx
+++ app/Segunda-secao/SegundaSecao.tsx
@@ -1,10 +1,21 @@
 
 import Image from "next/image";
 import img1 from "@/public/primeiraimagem.jpeg";
+import img2 from "@/public/segundaimagem.jpeg";
+import img3 from "@/public/terceiraimagem.jpeg";
+import img4 from "@/public/quartaimagem.jpeg";
+import img5 from "@/public/quintaimagem.jpeg";
+import img6 from "@/public/sextaimagem.jpeg";
+import img7 from "@/public/setimaimagem.jpeg";
+import img8 from "@/public/oitavaimagem.jpeg";
+import img9 from "@/public/nonaimagem.jpeg";
+import img10 from "@/public/decimaimagem.jpeg";
+import img11 from "@/public/decimaprimeiraimagem.jpeg";
+import img12 from "@/public/decimasegundaimagem.jpeg";
 //import img2 from "@/public/segunda.jpeg"; // se tiver outras, coloque
 // você pode repetir o mesmo img1 várias vezes se só tiver uma
 
-const imagens = [img1, img1, img1, img1, img1]; // coloque quantas quiser / ou troque por img2, img3...
+const imagens = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12]; // coloque quantas quiser / ou troque por img2, img3...
 
 export default function SegundaSecao() {
   // duplicamos o array para fazer o loop contínuo
